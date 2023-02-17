fb7d5badce797384a639b84676fc069842467665
LudoTreb <l.trebalag@gmail.com>
2023-02-18 00:11:28 +0100

    create and config the devise app

--- src/DashboardDevise/urls.py
+++ src/DashboardDevise/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from devise.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", dashboard),
 ]
